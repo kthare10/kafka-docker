d27ebc6d17af63f72b55d8494fbbee8de104638b
Komal Thareja <kthar10@renci.org>
2019-01-11 10:06:24 -0500

    fix for override params

--- 2.11-2.0.1/docker-entrypoint.sh
+++ 2.11-2.0.1/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties --override log.dirs=/var/log/kafka-logs zookeeper.connect=$ZOOKEEPER_CONNECT advertised.listeners=$KAFKA_LISTENERS
+/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties --override log.dirs=/var/log/kafka-logs --override zookeeper.connect=$ZOOKEEPER_CONNECT --override advertised.listeners=$KAFKA_LISTENERS
 
 exec "$@"
